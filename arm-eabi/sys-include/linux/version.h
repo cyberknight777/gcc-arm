0f46f9c73c3a2f3687965809ebaabe87d18966ae
cyberknight777 <cyberknight755@gmail.com>
2022-06-03 19:38:55 +0530

    EvaGCC: Import arm GCC 20220603
    Build completed on: Fri Jun  3 19:38:55 IST 2022
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/arm-eabi-gcc
    COLLECT_LTO_WRAPPER=/tmp/gcc-arm/bin/../libexec/gcc/arm-eabi/12.1.1/lto-wrapper
    Target: arm-eabi
    Configured with: ../gcc/configure --target=arm-eabi CFLAGS='-flto -flto-compression-level=10 -O3 -pipe -ffunction-sections -fdata-sections' CXXFLAGS='-flto -flto-compression-level=10 -O3 -pipe -ffunction-sections -fdata-sections' --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx --disable-nls --disable-shared --enable-default-ssp --enable-languages=c --enable-threads=posix --prefix=/tmp/cirrus-ci-build/../gcc-arm --with-glibc-version=2.27 --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-pkgversion='Eva GCC' --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib zstd
    gcc version 12.1.1 20220602 (Stable) (Eva GCC)
    
    LLD: LLD 15.0.0 (compatible with GNU linkers)
    Signed-off-by: cyberknight777 <cyberknight755@gmail.com>

---
+++ arm-eabi/sys-include/linux/version.h
@@ -0,0 +1,10 @@
+#define LINUX_VERSION_CODE 332288
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + ((c) > 255 ? 255 : (c)))
+#define LINUX_VERSION_MAJOR 5
+#define LINUX_VERSION_PATCHLEVEL 18
+#define LINUX_VERSION_SUBLEVEL 0
+#define RHEL_MAJOR 9
+#define RHEL_MINOR 99
+#define RHEL_RELEASE_VERSION(a,b) (((a) << 8) + (b))
+#define RHEL_RELEASE_CODE 2403
+#define RHEL_RELEASE "60"
